bb9842b7bf1d2ba1278760acb3099463b22a18db
2025rd058-bit <2025RD058@utez.edu.mx>
2026-02-25 01:41:19 -0600

    cree una clase Animal

---
+++ Maiiin.java
@@ -0,0 +1,14 @@
+public class Maiiin {
+    public static void main(String[] args) {
+
+        Animal animal1 = new Animal("Animal Genérico", 5);
+        Animal animal2 = new Perro("Max", 3, "Labrador");
+
+        System.out.println("=== Animal Normal ===");
+        animal1.hacerSonido();
+
+        System.out.println("\n=== Perro ===");
+        animal2.hacerSonido(); 
+    }}
+    
+
